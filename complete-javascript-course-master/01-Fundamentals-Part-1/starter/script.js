793ebc6f502fd3f44800ff4a7e561257dc6b1001
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-04-11 17:47:37 +0000

    data types

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = 'amazing';
+/*let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
@@ -11,3 +11,19 @@ let myFirstJob = "Teacher";
 let myCurrentJob = "Programmer";
 console.log(myFirstJob);
 console.log(myCurrentJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
