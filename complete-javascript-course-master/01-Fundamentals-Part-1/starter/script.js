eef85d85047662f11c2147fdbe06c00e3b92964c
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-04-13 15:36:09 +0000

    coding challenge

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -32,7 +32,7 @@ let age = 30;
 age = 31;
 
 const birthYear = 1990;
-*/
+
 
 //Math operators
 const now = 2037
@@ -65,3 +65,40 @@ const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
 
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = ageJonas + ageSarah / 2
+console.log(ageJonas, ageSarah, averageAge);
+*/
+
+//Data 1
+const markMassData1 = 78;
+const markHeightData1 = 1.69;
+const johnMassData1 = 92;
+const johnHeightData1 = 1.95;
+
+const bmiMarkData1 = markMassData1 / markHeightData1 ** 2;
+const bmiJohnData1 = johnMassData1 / johnHeightData1 ** 2;
+
+const markHigherBMIData1 = bmiMarkData1 > bmiJohnData1
+
+console.log(markHigherBMIData1)
+//Data 2
+const markMassData2 = 95;
+const markHeightData2 = 1.88;
+const johnMassData2 = 85;
+const johnHeightData2 = 1.76;
+
+const bmiMarkData2 = markMassData2 / markHeightData2 ** 2;
+const bmiJohnData2 = johnMassData2 / johnHeightData2 ** 2;
+
+const markHigherBMIData2 = bmiMarkData2 > bmiJohnData2
+
+console.log(markHigherBMIData2)
