8dedb4ddc37ecbea4e56420b4c5a03ca31051f7b
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-04-13 14:34:53 +0000

    basic operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -27,9 +27,41 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 let age = 30;
 age = 31;
 
 const birthYear = 1990;
+*/
+
+//Math operators
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Bernard";
+const lastName = "Ansah";
+console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+console.log(x);
+
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+console.log(x);
+
+//Comparison operators
+// We use comparison operators to produce boolean values
+console.log(ageJonas > ageSarah); // >, <. >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
