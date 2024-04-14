778a535beceb19808069e7da2f85b5b777456b90
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-04-14 01:20:47 +0000

    type conversion and coercion

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -154,3 +154,19 @@ if (bmiMarkData1 > bmiJohnData1) {
   console.log(`John's BMI ${bmiJohnData1} is higher than Mark's ${bmiMarkData1}!`)
 }
 */
+
+//type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear); 
+console.log(Number(inputMonth) + 18); 
+
+console.log(Number('Bernard'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
