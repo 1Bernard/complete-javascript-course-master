0d34ffab072ba19a6148ff13af7127f3ba625907
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-05-30 10:55:54 +0000

    truthy and falsy values

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -156,17 +156,45 @@ if (bmiMarkData1 > bmiJohnData1) {
 */
 
 //type conversion
-const inputYear = '1991';
-console.log(Number(inputYear), inputYear); 
-console.log(Number(inputMonth) + 18); 
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputMonth) + 18);
 
-console.log(Number('Bernard'));
-console.log(typeof NaN);
+// console.log(Number('Bernard'));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-//type coercion
-console.log('I am ' + 23 + ' years old');
-console.log('23' - '10' - 3);
-console.log('23' / '2');
+// //type coercion
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' / '2');
 
+// 5 falsy values in JavaScript: 0, '', undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Bernard'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money = 0;
+
+// if (money) {
+//   console.log("Don't spend it all;");
+// } else {
+//   console.log('You should get a job!');
+// }
+
+// let height;
+
+// if (height) {
+//   console.log('YAY! Height is defined');
+// } else {
+//   console.log('Height is undefined');
+// }
+
+const age = '18';
+
+if (age === 18) console.log('You just became an adult (strict)');
+
+if (age == '18') console.log('You just became an adult  (loose)');
