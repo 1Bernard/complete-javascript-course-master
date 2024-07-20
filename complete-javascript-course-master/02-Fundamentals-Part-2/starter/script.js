281a9624d49c61866bd4aaff2f296d751a16c139
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-07-20 19:58:57 +0000

    function declaration

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// let hasDriversLicence = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicence = true;
+// if (hasDriversLicence) console.log('I can drive');
+
+// function logger() {
+//   console.log('My name is Bernard');
+// }
+
+// //calling, running, invoking
+// logger();
+// logger();
+// logger();
+
+// function foodProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = foodProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = foodProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
